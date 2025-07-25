6ff3d52f87979399a39a6de345965e0b64bc502c
Niloufar Saeidi Mobarakeh <nilsa@narval2.narval.calcul.quebec>
2025-07-25 02:31:11 -0400

    multi head self-attention

--- model/attention.py
+++ model/attention.py
@@ -37,7 +37,7 @@ def scaled_dot_product_attention(q, k, v, mask=None):
     scaled_scores = scores/math.sqrt(k.size(-1)) # tensor.size(dim0) gets the size of dimention dim0 of the tensor| k.size(-1) returns an int but torch.sqrt expects a tensor, so if used we should convert:  torch.sqrt(torch.tensor(k.size(-1), dtype=torch.float32))
     # math.sqrt expects int so no problem there
     # scaled_scores = scaled_scores - scaled_scores.max(dim=-1, keepdim=True).values 
-    if mask is None:
+    if mask is None: #always autoregressive for now
         mask = torch.tril(torch.ones([q.size(0), q.size(1), q.size(1)]), diagonal=0) # the tokens do attend to themselves bc diagonal is also 1 (by setting diagonal = 0!)- the syntax looks confusing to me 
     scaled_scores = scaled_scores.masked_fill(mask == 0, -float('inf'))
     attn_weights = torch.softmax(scaled_scores, dim = -1) # we apply softmax to the key dimension so that For each query, all key weights sum to 1
@@ -72,35 +72,35 @@ class SingleHeadSelfAttention(nn.Module):
         # residul connection also might help the stability of the learning by having the unchanged gradients (kind of like a shortcut)
         # also attention output might discard useful raw information - just in case!
         return self.layer_norm(attn_out) # adding LayerNorm helps with sclae consistency and speeds up learnign convergence
+    
+class MultiHeadSelfAttention(nn.Module):
+    def __init__(self, embed_dim, num_heads):
+        super().__init__()
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.q_projection_layer = nn.Linear(embed_dim, embed_dim) 
+        self.k_projection_layer = nn.Linear(embed_dim, embed_dim) 
+        self.v_projection_layer = nn.Linear(embed_dim, embed_dim) 
+        self.final_projection_layer = nn.Linear(embed_dim, embed_dim) 
+        self.embed_dim = embed_dim
+        self.layer_norm = nn.LayerNorm(self.embed_dim)
         
-    class MultiHeadSelfAttention(nn.Module):
-        def __init__(self, embed_dim, num_heads):
-            super().__init__()
-            self.embed_dim = embed_dim
-            self.num_heads = num_heads
-            self.q_projection_layer = nn.Linear(embed_dim, embed_dim) 
-            self.k_projection_layer = nn.Linear(embed_dim, embed_dim) 
-            self.v_projection_layer = nn.Linear(embed_dim, embed_dim) 
-            self.final_projection_layer = nn.Linear(embed_dim, embed_dim) 
-            self.embed_dim = embed_dim
-            self.layer_norm = nn.LayerNorm(self.embed_dim)
-            
-        def forward(self, input):
-            q = self.q_projection_layer(input)
-            k = self.k_projection_layer(input)
-            v = self.v_projection_layer(input)
-            [batch_size, seq_len, embed_dim] = q.size() # or batch_size, seq_len, embed_dim = q.size() - they both work
-            assert embed_dim % self.num_heads == 0
-            head_dim = int(embed_dim//self.num_heads)
-            q = q.reshape([batch_size, seq_len, self.num_heads, head_dim]).transpose(1, 2)
-            k = k.reshape([batch_size, seq_len, self.num_heads, head_dim]).transpose(1, 2)
-            v = v.reshape([batch_size, seq_len, self.num_heads, head_dim]).transpose(1, 2)
-            
-            attn_out = scaled_dot_product_attention(q, k, v)
-            # for head in self.num_heads:  prevents GPU from parallelizing operations efficiently and introduces slow Python-level control flow.
-            #     attn_out = scaled_dot_product_attention(q[:, :, head, :], k[:, :, head, :], v[:, :, head, :])
-            attn_out = attn_out.transpose(1, 2).reshape([batch_size, seq_len, embed_dim])
-            assert attn_out.shape == input.shape
-            attn_out = self.final_projection_layer(attn_out)
-            attn_out = attn_out + input 
-            return self.layer_norm(attn_out) 
+    def forward(self, input):
+        q = self.q_projection_layer(input)
+        k = self.k_projection_layer(input)
+        v = self.v_projection_layer(input)
+        [batch_size, seq_len, embed_dim] = q.size() # or batch_size, seq_len, embed_dim = q.size() - they both work
+        assert embed_dim % self.num_heads == 0
+        head_dim = int(embed_dim//self.num_heads)
+        q = q.reshape([batch_size, seq_len, self.num_heads, head_dim]).transpose(1, 2)
+        k = k.reshape([batch_size, seq_len, self.num_heads, head_dim]).transpose(1, 2)
+        v = v.reshape([batch_size, seq_len, self.num_heads, head_dim]).transpose(1, 2)
+        
+        attn_out = scaled_dot_product_attention(q, k, v)
+        # for head in self.num_heads:  prevents GPU from parallelizing operations efficiently and introduces slow Python-level control flow.
+        #     attn_out = scaled_dot_product_attention(q[:, :, head, :], k[:, :, head, :], v[:, :, head, :])
+        attn_out = attn_out.transpose(1, 2).reshape([batch_size, seq_len, embed_dim])
+        assert attn_out.shape == input.shape
+        attn_out = self.final_projection_layer(attn_out)
+        attn_out = attn_out + input 
+        return self.layer_norm(attn_out) 
