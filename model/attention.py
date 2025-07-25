7d589dea0d636cd199fa95b052c7fb00bb7c54b1
Niloufar Saeidi Mobarakeh <nilsa@narval2.narval.calcul.quebec>
2025-07-25 02:27:06 -0400

    multi head self-attention

--- model/attention.py
+++ model/attention.py
@@ -27,7 +27,7 @@ import math
     #  (2) To prevent looking into the future in causal/self-attention
     # Then when we do softmax on top of that, softmax(-inf) → 0 so we dont attend to them
 def scaled_dot_product_attention(q, k, v, mask=None): 
-    # k looks like (batch_size, seq_len_k, d_k)
+    # k looks like (batch_size, seq_len_k, d_k)  - unless multihead ->  (batch_size,num_heads, seq_len_k, d_k)
     # q looks like (batch_size, seq_len_q, d_k)
     # if we do k.transpose(-2, -1), we get (batch_size, d_k, seq_len_k)
     # so after the multiplication, we will have (batch_size, seq_len_q, seq_len_k)
@@ -73,4 +73,34 @@ class SingleHeadSelfAttention(nn.Module):
         # also attention output might discard useful raw information - just in case!
         return self.layer_norm(attn_out) # adding LayerNorm helps with sclae consistency and speeds up learnign convergence
         
-    
+    class MultiHeadSelfAttention(nn.Module):
+        def __init__(self, embed_dim, num_heads):
+            super().__init__()
+            self.embed_dim = embed_dim
+            self.num_heads = num_heads
+            self.q_projection_layer = nn.Linear(embed_dim, embed_dim) 
+            self.k_projection_layer = nn.Linear(embed_dim, embed_dim) 
+            self.v_projection_layer = nn.Linear(embed_dim, embed_dim) 
+            self.final_projection_layer = nn.Linear(embed_dim, embed_dim) 
+            self.embed_dim = embed_dim
+            self.layer_norm = nn.LayerNorm(self.embed_dim)
+            
+        def forward(self, input):
+            q = self.q_projection_layer(input)
+            k = self.k_projection_layer(input)
+            v = self.v_projection_layer(input)
+            [batch_size, seq_len, embed_dim] = q.size() # or batch_size, seq_len, embed_dim = q.size() - they both work
+            assert embed_dim % self.num_heads == 0
+            head_dim = int(embed_dim//self.num_heads)
+            q = q.reshape([batch_size, seq_len, self.num_heads, head_dim]).transpose(1, 2)
+            k = k.reshape([batch_size, seq_len, self.num_heads, head_dim]).transpose(1, 2)
+            v = v.reshape([batch_size, seq_len, self.num_heads, head_dim]).transpose(1, 2)
+            
+            attn_out = scaled_dot_product_attention(q, k, v)
+            # for head in self.num_heads:  prevents GPU from parallelizing operations efficiently and introduces slow Python-level control flow.
+            #     attn_out = scaled_dot_product_attention(q[:, :, head, :], k[:, :, head, :], v[:, :, head, :])
+            attn_out = attn_out.transpose(1, 2).reshape([batch_size, seq_len, embed_dim])
+            assert attn_out.shape == input.shape
+            attn_out = self.final_projection_layer(attn_out)
+            attn_out = attn_out + input 
+            return self.layer_norm(attn_out) 
