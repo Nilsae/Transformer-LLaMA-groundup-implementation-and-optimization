edeb00d1fb041bd5860cbee34ebd8716480d1d42
Niloufar Saeidi Mobarakeh <nilsa@narval1.narval.calcul.quebec>
2025-07-26 19:22:20 -0400

    added padding mask

--- model/transformer.py
+++ model/transformer.py
@@ -26,26 +26,33 @@ class MultiHeadSelfAttention(nn.Module):
         self.is_autoregressive = is_autoregressive
         self.dropout = nn.Dropout(dropout_rate)
     
-    def scaled_dot_product_attention(self, q, k, v, mask=None, past_k = None, past_v = None, is_autoregressive= True): #(batch_size, seq_len_k, d_k) or (batch_size,num_heads, seq_len_k, d_k)
+    def scaled_dot_product_attention(self, q, k, v, causal_mask=None, padding_mask = None, past_k = None, past_v = None, is_autoregressive= True): #(batch_size, seq_len_k, d_k) or (batch_size,num_heads, seq_len_k, d_k)
         if past_k is not None and past_v is not None:
             assert past_k.shape == past_v.shape
             k = torch.cat([past_k, k], dim = -2) # torch.cat([tensor1, tensor2, ...])
             v = torch.cat([past_v, v], dim = -2)
         scores = torch.matmul(q, k.transpose(-2, -1))
         scaled_scores = scores/math.sqrt(k.size(-1)) 
+        
         if is_autoregressive:
-            if mask is None: 
+            if causal_mask is None: 
                 seq_len_q = scores.size(-2) # negative because num_heads might be or not be there
                 seq_len_k = scores.size(-1)
-                mask = torch.tril(torch.ones(seq_len_q, seq_len_k, device=q.device)).bool()
-                mask = mask.unsqueeze(0).unsqueeze(0)  # shape (1, 1, seq_len_q, seq_len_k) 1 -> all the batches
-            scaled_scores = scaled_scores.masked_fill(mask == 0, -float('inf'))
+                causal_mask = torch.tril(torch.ones(seq_len_q, seq_len_k, device=q.device)).bool()
+                causal_mask = causal_mask.unsqueeze(0).unsqueeze(0)  # shape (1, 1, seq_len_q, seq_len_k) 1 -> all the batches
+        if is_autoregressive:
+            if padding_mask is not None:
+                combined_mask = padding_mask & causal_mask
+                scaled_scores = scaled_scores.masked_fill(combined_mask == 0, -float('inf'))
+            else:
+                scaled_scores = scaled_scores.masked_fill(causal_mask == 0, -float('inf'))
+        
         scaled_scores = scaled_scores - scaled_scores.max(dim=-1, keepdim=True).values #Prevents softmax overflow by centering scores.  # not in the original transformer paper
         attn_weights = torch.softmax(scaled_scores, dim = -1) 
         attn_weights_dropout = self.dropout(attn_weights)
         return torch.matmul(attn_weights_dropout, v)
     
-    def forward(self, input, past_k=None, past_v=None):
+    def forward(self, input, past_k=None, past_v=None, padding_mask = None):
         q = self.q_projection_layer(input)
         k = self.k_projection_layer(input)
         v = self.v_projection_layer(input)
@@ -55,8 +62,7 @@ class MultiHeadSelfAttention(nn.Module):
         q = q.reshape([batch_size, seq_len, self.num_heads, head_dim]).transpose(1, 2)
         k = k.reshape([batch_size, seq_len, self.num_heads, head_dim]).transpose(1, 2)
         v = v.reshape([batch_size, seq_len, self.num_heads, head_dim]).transpose(1, 2)
-        
-        attn_out = self.scaled_dot_product_attention(q, k, v, mask = None, past_k = past_k, past_v = past_v, is_autoregressive = self.is_autoregressive)
+        attn_out = self.scaled_dot_product_attention(q, k, v, causal_mask = None, padding_mask=padding_mask, past_k = past_k, past_v = past_v, is_autoregressive = self.is_autoregressive)
         # for head in self.num_heads:  prevents GPU from parallelizing operations efficiently and introduces slow Python-level control flow.
         #     attn_out = scaled_dot_product_attention(q[:, :, head, :], k[:, :, head, :], v[:, :, head, :])
         attn_out = attn_out.transpose(1, 2).reshape([batch_size, seq_len, embed_dim])
@@ -77,13 +83,15 @@ class CrossAttention(nn.Module):
         self.final_projection_layer = nn.Linear(embed_dim, embed_dim) 
         self.embed_dim = embed_dim
         self.dropout = nn.Dropout(dropout_rate)
-    def cross_attention(self,q, k, v, past_k = None, past_v = None): 
+    def cross_attention(self,q, k, v, past_k = None, past_v = None, padding_mask = None): 
         if past_k is not None and past_v is not None:
             assert past_k.shape == past_v.shape
             k = torch.cat([past_k, k], dim = -2) # torch.cat([tensor1, tensor2, ...])
             v = torch.cat([past_v, v], dim = -2)
         scores = torch.matmul(q, k.transpose(-2, -1))
         scaled_scores = scores/math.sqrt(k.size(-1)) 
+        if padding_mask is not None:
+                scaled_scores = scaled_scores.masked_fill(padding_mask == 0, -float('inf'))
         scaled_scores = scaled_scores - scaled_scores.max(dim=-1, keepdim=True).values 
         attn_weights = torch.softmax(scaled_scores, dim = -1) 
         attn_weights_dropout = self.dropout(attn_weights)
