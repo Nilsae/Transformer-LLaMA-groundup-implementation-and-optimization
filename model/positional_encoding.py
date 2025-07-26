ef7ddc74ea0d84468bd91d4b48c69392812f9b77
Niloufar Saeidi Mobarakeh <nilsa@narval2.narval.calcul.quebec>
2025-07-26 01:51:14 -0400

    positional encoding sinusidal

---
+++ model/positional_encoding.py
@@ -0,0 +1,25 @@
+import torch, math
+
+
+def sinusoidal_encoding(batch_size, seq_len, embed_dim):
+    
+    # conceptually:
+    out  = torch.zeros(seq_len, embed_dim)
+    for pos in range(seq_len):
+        for dim in range(embed_dim):
+            if dim%2 ==0:
+                out[pos][dim] = math.sin(pos/10000**(2*(dim//2)/embed_dim))
+            else:
+                out[pos][dim] = math.cos(pos/10000**(2*(dim//2)/embed_dim))
+    
+    # properly;
+    
+    pos = torch.tensor([[i] for i in range(seq_len)])
+    i = torch.arange(0, embed_dim//2, 1)
+    pos_encoding = torch.zeros(seq_len, embed_dim)    
+    divisor_term = 10000**(2 * i / embed_dim)
+    matrix = pos/divisor_term
+    pos_encoding[:, 0::2] = torch.sin(matrix) # first dim is batch sizes
+    pos_encoding[:, 1::2] = torch.cos(matrix)
+    out = pos_encoding.unsqueeze(0).expand(batch_size, -1, -1)
+    return out
